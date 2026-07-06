ff2ef8efdff31c24118f0be1a5f92a4448ade468
Chindarat <buytaxusa@gmail.com>
2026-07-06 17:08:10 +0700

    update my web

--- appdemo/settings.py
+++ appdemo/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#p(itj=g^yp703*p7o9fe-1kvqw_$388$9#80ta&isnxj(luz3"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-#p(itj=g^yp703*p7o9fe-1kvqw_$388$9#80ta&isnxj(luz3")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True" if not os.environ.get("VERCEL") else "False") == "True"
 
 ALLOWED_HOSTS = [".vercel.app", "now.sh", "127.0.0.1", "localhost"]
 
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'web'
 ]
 
 MIDDLEWARE = [
